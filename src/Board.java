a7ead11c041a79a7382d141a354e2557084a0f4c
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-03-27 00:52:06 +1100

    code cleanup, rearrange for efficiency, introduce checkTile()

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,7 @@ public class Board {
 	public int n; // The N value
 	public int size; // The size of the board (4*n-1)
 	public Tile[][] board;
-	public int possibleMoves = 0; // Number of possible moves at this state
+	public int possibleMoves; // Number of possible moves at this state
 	public int maxByOneMove = 0;  // Maximum number of hexagonal cells that can be captured by one move (0, 1, 2)
 	public int avlbCaptures = 0; // Number of hexagonal cells available for capture by a single move
 	
@@ -37,6 +37,11 @@ public class Board {
 						throw new IllegalArgumentException();
 					
 					board[i][j].setCharValue(value);
+					
+					// Populate the total @possibleMoves int
+					possibleMoves = 0;
+					if (value == '+')
+						possibleMoves++;
 				}
 			}
 		} catch (InputMismatchException|IllegalArgumentException e) {
@@ -47,6 +52,11 @@ public class Board {
 		scan.close();
 		
 		determineCaptureValues();
+		
+		// By this point, the following have been initialized
+		// for this instance of Board:
+		// Tile board[][] (Each Tile object has charValue and captureValue)
+		// int n, size, possibleMoves
 		
 		printBoard();
 	}
@@ -64,42 +74,57 @@ public class Board {
 			System.out.println();
 		}
 		System.out.println();
-	}	
+	}
 	
-	
-	
-	/** Count the possible moves
+	/**
+	 * Given a tile point using i, j, returns true if the tile coordinate is valid
+	 * Valid tiles are tiles that can have +, R or G on it.
 	 * 
-	 * @return the number of possible Moves
+	 * Hexagon centers and boundaries therefore return false
 	 */
-	public int countPossibleMoves(){
-		int i, j;
-		possibleMoves = 0;
+	public Boolean checkTile(int i, int j){
 		
-		for(i = 0; i< size; i++){
-			for(j = 0; j< size; j++){
-				if (board[i][j].getCharValue() == '+'){
-					possibleMoves++;
-				}
-			}
-		}
-		return possibleMoves;	
+		// Check for array boundary
+		if (i >= size || j >= size)
+			return false;
+		
+		// Check for hexagon boundary
+		// Note: Center of hexagons only occur on odd i, j
+		if (i%2==1 && j%2==1)
+			return false;
+		
+		// Check for game boundary
+		// If j < 2n-1, then for tile to be valid i <= (2n-1)+j
+		// If j > 2n-1, then for tile to be valid i >= j-(2n-1)
+		// If j = 2n-1, then tile is valid
+		int boundary = 2*n-1;
+		if((j < boundary && i <= boundary+j)||(j > boundary && i >= j-boundary)||(j == boundary))
+			return true;
+		
+		return false;
 	}
 	
 	
 	
+	/** Return the possible moves
+	 * 
+	 * Note: The iterator for counting the possible number of moves
+	 * is within buildBoard() in order to reduce the Big-O complexity 
+	 * to 1 after initialization.
+	 * 
+	 * @return the number of possible Moves
+	 */
+	public int possibleMoves(){
+		return possibleMoves;
+	}
 	
 	/** Determine the capture value of every tile in the board
 	 */
 	public void determineCaptureValues(){
-		int i, j;
-		for (i = 0; i< size; i+=2){
-			for(j =0; j< size; j+=2){
-				if (board[i][j].getCharValue() != '-'){
+		for(int i=0; i<size; i++)
+			for(int j=0; j<size; j++)
+				if (checkTile(i, j))
 					determineCaptureValue(i, j);
-				}	
-			}
-		}
 	}
 	
 	
@@ -115,10 +140,11 @@ public class Board {
 		int iIncrease = 0, jIncrease = 0;
 		
 		for(k = 0; k<jValues.length; k++){
-			if (iValues[k] >= size || jValues[k] >= size){
+			if(!checkTile(iValues[k], jValues[k])){
 				isOutOfBound = true;
 				break;
 			}
+
 			if(board[iValues[k]][jValues[k]].getCharValue() == '+'){
 				iIncrease = iValues[k];
 				jIncrease = jValues[k];
@@ -165,10 +191,4 @@ public class Board {
 		}
 		return maxByOneMove; 
 	}
-	
-	
-	
-	
-	
-	
 }
--- src/TestBoard.java
+++ src/TestBoard.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class TestBoard {
 	public static void main(String[] args){
 		Board board = new Board();
-		System.out.println(board.countPossibleMoves());
+		System.out.println(board.possibleMoves());
 		System.out.println(board.countMaxByOneMove());
 		System.out.println(board.countAvailableCaptures());
 	}
