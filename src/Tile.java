a35d26ff2430d94ccae2d110ce151ce9a8c0f106
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-03-26 11:53:10 +1100

    added tile class

---
+++ src/Tile.java
@@ -0,0 +1,21 @@
+
+public class Tile {
+	public Point location;
+	public char type;
+	
+	public Tile(Point tileLocation, char typeOfTile){
+		type = typeOfTile;
+		location = tileLocation;
+	}
+	
+	public Point getTileLocation() {
+		return location;
+	}
+	
+	public char getTileValue() {
+		return type;
+	}
+	
+	public int isAvailable(int boardSize) {
+	}
+}
